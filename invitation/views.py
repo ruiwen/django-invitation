a8ccb0ea7e7087da55609727ff5c92cd3c8e024e
David Larlet <larlet@gmail.com>
2008-12-09 16:11:23 +0100

    Add useful keys in context

--- invitation/views.py
+++ invitation/views.py
@@ -18,7 +18,7 @@ def invited(request, invitation_key=None):
             template = 'invitation/invited.html'
         else:
             template = 'invitation/wrong_invitation_key.html'
-        return direct_to_template(request, template)
+        return direct_to_template(request, template, {'invitation_key': invitation_key})
     else:
         return HttpResponseRedirect(reverse('registration_register'))
 
@@ -29,6 +29,10 @@ def register(request, success_url=None,
     if 'INVITE_MODE' in settings.get_all_members() and settings.INVITE_MODE:
         if 'invitation_key' in request.REQUEST \
             and is_key_valid(request.REQUEST['invitation_key']):
+            if extra_context is None:
+                extra_context = {'invitation_key': request.REQUEST['invitation_key']}
+            else:
+                extra_context.update({'invitation_key': invitation_key})
             return registration_register(request, success_url, form_class, 
                         profile_callback, template_name, extra_context)
         else:
@@ -52,5 +56,8 @@ def invite(request, success_url=None,
             return HttpResponseRedirect(success_url or reverse('invitation_complete'))
     else:
         form = form_class()
-    return direct_to_template(request, template_name, {'form': form})
+    return direct_to_template(request, template_name, {
+        'form': form,
+        'remaining_invitations': InvitationKey.objects.remaining_invitations_for_user(request.user),
+    })
 invite = login_required(invite)
