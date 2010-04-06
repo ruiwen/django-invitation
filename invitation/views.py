cd3fc03dc8b996bdc0d093189de88ed842dba850
"Brent O'Connor" <epicserve@gmail.com>
2010-04-06 15:53:06 -0500

    Made it so it now marks invites as being used when working with the master branch of django-registration.
    Removed the InvitationUsedCallback and replaced it with RegistrationBackend
    class that overrides registration.backends.default. A better solution would
    be to get whatever the current backend is and override the
    post_registration_redirect method.

--- invitation/views.py
+++ invitation/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 from registration.views import register as registration_register
 from registration.forms import RegistrationForm
+from registration.backends import default as registration_backend
 
 from invitation.models import InvitationKey
 from invitation.forms import InvitationKeyForm
@@ -15,24 +16,21 @@ remaining_invitations_for_user = InvitationKey.objects.remaining_invitations_for
 
 # TODO: move the authorization control to a dedicated decorator
 
-class InvitationUsedCallback(object):
-    """
-    Callable to mark InvitationKey as used, by way of profile_callback.
-    """
-    def __init__(self, invitation_key, profile_callback):
-        self.invitation_key = invitation_key
-        self.profile_callback = profile_callback
-        
-    def __call__(self, user):
-        """Mark the key used, and then call the real callback (if any)."""
-        key = InvitationKey.objects.get_key(self.invitation_key)
+class RegistrationBackend(registration_backend.DefaultBackend):
+
+    def post_registration_redirect(self, request, user, *args, **kwargs):
+        """
+        Return the name of the URL to redirect to after successful
+        user registration.
+
+        """
+        invitation_key = request.REQUEST.get('invitation_key')
+        key = InvitationKey.objects.get_key(invitation_key)
         if key:
             key.mark_used(user)
 
-        if self.profile_callback:
-            self.profile_callback(user)
+        return ('registration_complete', (), {})
 
-            
 def invited(request, invitation_key=None, extra_context=None):
     if 'INVITE_MODE' in dir(settings) and settings.INVITE_MODE:
         if invitation_key and is_key_valid(invitation_key):
@@ -46,7 +44,9 @@ def invited(request, invitation_key=None, extra_context=None):
         return HttpResponseRedirect(reverse('registration_register'))
 
 def register(request, backend=None, success_url=None,
-            form_class=RegistrationForm, profile_callback=None,
+            form_class=RegistrationForm,
+            disallowed_url='registration_disallowed',
+            post_registration_redirect=None,
             template_name='registration/registration_form.html',
             extra_context=None):
     extra_context = extra_context is not None and extra_context.copy() or {}
@@ -55,10 +55,12 @@ def register(request, backend=None, success_url=None,
             invitation_key = request.REQUEST['invitation_key']
             extra_context.update({'invitation_key': invitation_key})
             if is_key_valid(invitation_key):
-                profile_callback = InvitationUsedCallback(invitation_key, 
-                                                          profile_callback)
-                return registration_register(request, backend, success_url, form_class, 
-                            profile_callback, template_name, extra_context)
+                # import ipdb; ipdb.set_trace()
+                backend='invitation.views.RegistrationBackend'
+                return registration_register(request, backend, success_url,
+                                            form_class,
+                                            disallowed_url,
+                                            template_name, extra_context)
             else:
                 extra_context.update({'invalid_key': True})
         else:
@@ -66,12 +68,12 @@ def register(request, backend=None, success_url=None,
         template_name = 'invitation/wrong_invitation_key.html'
         return direct_to_template(request, template_name, extra_context)
     else:
-        return registration_register(request, success_url, form_class, 
+        return registration_register(request, success_url, form_class,
                             profile_callback, template_name, extra_context)
 
-def invite(request, success_url=None, 
+def invite(request, success_url=None,
             form_class=InvitationKeyForm,
-            template_name='invitation/invitation_form.html', 
+            template_name='invitation/invitation_form.html',
             extra_context=None):
     extra_context = extra_context is not None and extra_context.copy() or {}
     remaining_invitations = remaining_invitations_for_user(request.user)
